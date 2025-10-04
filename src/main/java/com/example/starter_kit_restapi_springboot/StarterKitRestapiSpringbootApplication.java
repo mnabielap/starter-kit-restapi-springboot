773da3162e883ef06e2fde8827c7761b2f686f00
Muhammad Nabiel Andityo <101865006+mnabielap@users.noreply.github.com>
2025-10-04 15:40:35 +0700

    initial project

---
+++ src/main/java/com/example/starter_kit_restapi_springboot/StarterKitRestapiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.starter_kit_restapi_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterKitRestapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterKitRestapiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/starter_kit_restapi_springboot/StarterKitRestapiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starter_kit_restapi_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterKitRestapiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
